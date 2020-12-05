4b34e2fb3e12cfa5572412bca5064988a759cf45
leeyoungjae2001 <57875002+leeyoungjae2001@users.noreply.github.com>
2020-12-06 01:26:22 +0900

    a

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.onload = function () {
   var right_control = document.getElementById("right");
   var now_slide=0
 
-  left_control.onclick = function () {
+  left_control.onclick = function(){
     var slidelist = document.getElementsByClassName("slidelist");
     var targets = slidelist[0].getElementsByTagName("li");
     if(now_slide == 0) now_slide = targets.length-1;
@@ -23,15 +23,50 @@ window.onload = function () {
     for(var i=0;i<targets.length;i++){
       targets[i].style["transform"] = "translateX("+(-100*now_slide) +"%)"
     }
-    
-    var targets_span = targets[now_slide].getElementsByTagName("span");
-    targets_span[0].style["fontSize"] = "0px";
-    for(var i=5; i < 80; i++){
-      setTimeout(() => {
-        targets_span[0].style["fontSize"] = i+"px";
-      }, i*100);
-      
-    }
   }
 
+  var body = document.getElementsByTagName("body");
+  body[0].addEventListener("mousewheel", function(delta){
+    if(delta.wheelDelta >= 0){
+      var slidelist = document.getElementsByClassName("slidelist");
+      var targets = slidelist[0].getElementsByTagName("li");
+      if(now_slide == 0) now_slide = targets.length-1;
+      else now_slide--;
+      
+      for(var i=0;i<targets.length;i++){
+        targets[i].style["transform"] = "translateX("+(-100*now_slide) +"%)" 
+      }
+    }else{
+      var slidelist = document.getElementsByClassName("slidelist");
+      var targets = slidelist[0].getElementsByTagName("li");
+      if(now_slide == targets.length-1) now_slide = 0;
+      else now_slide++;
+      
+      for(var i=0;i<targets.length;i++){
+        targets[i].style["transform"] = "translateX("+(-100*now_slide) +"%)"
+      }
+    }
+  })
+
+  body[0].onclick = function(a){
+    if(a.clientX < innerWidth/2){
+      var slidelist = document.getElementsByClassName("slidelist");
+      var targets = slidelist[0].getElementsByTagName("li");
+      if(now_slide == 0) now_slide = targets.length-1;
+      else now_slide--;
+      
+      for(var i=0;i<targets.length;i++){
+        targets[i].style["transform"] = "translateX("+(-100*now_slide) +"%)" 
+      }
+    }else{
+      var slidelist = document.getElementsByClassName("slidelist");
+      var targets = slidelist[0].getElementsByTagName("li");
+      if(now_slide == targets.length-1) now_slide = 0;
+      else now_slide++;
+      
+      for(var i=0;i<targets.length;i++){
+        targets[i].style["transform"] = "translateX("+(-100*now_slide) +"%)"
+      }
+    }
+  }
 };
